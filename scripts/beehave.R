0c727f644b658870577264c195b874d4a4e25611
AndreaGuarracino <andreaguarracino@outlook.com>
2021-02-13 14:00:08 +0100

    improved beehave script

--- scripts/beehave.R
+++ scripts/beehave.R
@@ -4,20 +4,50 @@ require(tidyverse)
 require(ggrepel)
 require(gridExtra)
 
-args <- commandArgs(trailingOnly=T)
+args <- commandArgs(trailingOnly = T)
 
-input.pgge <- read.delim(args[1])
-aln.id <- ggplot(input.pgge, aes(x=as.factor(cons.jump), y=aln.id, label=sample.name)) + 
-  geom_violin() + geom_point() + geom_text_repel() + xlab("consensus jump-max")
-qsc <- ggplot(input.pgge, aes(x=as.factor(cons.jump), y=qsc, label=sample.name)) + 
-  geom_violin() + geom_point() + geom_text_repel() + xlab("consensus jump-max") 
-uniq <- ggplot(input.pgge, aes(x=as.factor(cons.jump), y=uniq, label=sample.name)) + 
-  geom_violin() + geom_point() + geom_text_repel() + xlab("consensus jump-max")
-multi <- ggplot(input.pgge, aes(x=as.factor(cons.jump), y=multi, label=sample.name)) + 
-  geom_violin() + geom_point() + geom_text_repel() + xlab("consensus jump-max")
-nonaln <- ggplot(input.pgge, aes(x=as.factor(cons.jump), y=nonaln, label=sample.name)) + 
-  geom_violin() + geom_point() + geom_text_repel() + xlab("consensus jump-max")
-png(args[2], width = 1500, height = 500, pointsize = 25)
+input.pgge <- read.table(args[1], sep = '\t', header = T)
+
+aln.id <- ggplot(input.pgge, aes(x = as.factor(cons.jump), y = aln.id, label = sample.name)) +
+  geom_violin() +
+  geom_point() +
+  geom_text_repel(box.padding = 0.5, max.overlaps = Inf) +
+  xlab("consensus jump-max") +
+  theme(text = element_text(size = 16)) +
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.0001))
+
+qsc <- ggplot(input.pgge, aes(x = as.factor(cons.jump), y = qsc, label = sample.name)) +
+  geom_violin() +
+  geom_point() +
+  geom_text_repel(box.padding = 0.5, max.overlaps = Inf) +
+  xlab("consensus jump-max") +
+  theme(text = element_text(size = 16)) +
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.0001))
+
+uniq <- ggplot(input.pgge, aes(x = as.factor(cons.jump), y = uniq, label = sample.name)) +
+  geom_violin() +
+  geom_point() +
+  geom_text_repel(box.padding = 0.5, max.overlaps = Inf) +
+  xlab("consensus jump-max") +
+  theme(text = element_text(size = 16)) +
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.0001))
+
+multi <- ggplot(input.pgge, aes(x = as.factor(cons.jump), y = multi, label = sample.name)) +
+  geom_violin() +
+  geom_point() +
+  geom_text_repel(box.padding = 0.5, max.overlaps = Inf) +
+  xlab("consensus jump-max") +
+  theme(text = element_text(size = 16)) +
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.0001))
+
+nonaln <- ggplot(input.pgge, aes(x = as.factor(cons.jump), y = nonaln, label = sample.name)) +
+  geom_violin() +
+  geom_point() +
+  geom_text_repel(box.padding = 0.5, max.overlaps = Inf) +
+  xlab("consensus jump-max") +
+  theme(text = element_text(size = 16)) +
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.0001))
+
+png(args[2], width = 1700, height = 500, pointsize = 25)
 g <- grid.arrange(aln.id, qsc, uniq, multi, nonaln, nrow = 1)
 dev.off()
-
